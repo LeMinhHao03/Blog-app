07729e1d0cdb9433b6bbcfe944dfcebcb15a0b8d
LeMinhHao03 <haotp252gmail.com>
2024-04-11 22:10:00 +0700

    update

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BlogController extends Controller
+{
+    //tra ve tat ca blog
+    public function index()
+    {
+
+    }
+    //tra ve 1 blog
+    public function show()
+    {
+
+    }
+    //tra ve luu tru blog
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|min:10',
+            'author' => 'required|min:3'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Please fix the errors',
+                'errors' => $validator->errors()
+            ]);
+        }
+
+        $blog = new Blog();
+        $blog->title = $request->title;
+        $blog->author = $request->author;
+        $blog->description = $request->description;
+        $blog->shortDesc = $request->shortDesc;
+        $blog->save();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Blog added successfully',
+            'data' => $blog
+        ]);
+    }
+    //cap nhap cho blog
+    public function update()
+    {
+
+    }
+    //xoa cho blog
+    public function delete()
+    {
+
+    }
+}
